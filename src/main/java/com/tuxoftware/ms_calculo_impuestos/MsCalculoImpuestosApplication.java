2cbc75f754eb9f6ee6ccd29e8d51786e4d1b0ff6
Hugo Cobos <hcobos@azurian.com>
2025-12-29 18:36:06 -0600

    first commit

---
+++ src/main/java/com/tuxoftware/ms_calculo_impuestos/MsCalculoImpuestosApplication.java
@@ -0,0 +1,13 @@
+package com.tuxoftware.ms_calculo_impuestos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCalculoImpuestosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCalculoImpuestosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tuxoftware/ms_calculo_impuestos/MsCalculoImpuestosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuxoftware.ms_calculo_impuestos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsCalculoImpuestosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
