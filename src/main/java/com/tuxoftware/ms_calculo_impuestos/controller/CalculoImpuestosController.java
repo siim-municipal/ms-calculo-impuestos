7ce68670b1b5fb38d9950a6bdaf40d31360c71a6
Hugo Cobos <hcobos@azurian.com>
2026-01-05 09:31:46 -0600

    feat: Add JWT security integration with Keycloak and update API routes in ms-calculo-impuestos

--- src/main/java/com/tuxoftware/ms_calculo_impuestos/controller/CalculoImpuestosController.java
+++ src/main/java/com/tuxoftware/ms_calculo_impuestos/controller/CalculoImpuestosController.java
@@ -3,6 +3,7 @@ package com.tuxoftware.ms_calculo_impuestos.controller;
 import com.tuxoftware.ms_calculo_impuestos.dto.ResultadoCalculo;
 import com.tuxoftware.ms_calculo_impuestos.dto.SolicitudCalculo;
 import com.tuxoftware.ms_calculo_impuestos.service.CalculoService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
@@ -10,19 +11,17 @@ import org.springframework.web.bind.annotation.*;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/calculos")
+@RequestMapping("/api/v1/calculos")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "*") // Para desarrollo. En prod, especifica el dominio de Angular
 public class CalculoImpuestosController {
 
     private final CalculoService calculoService;
 
-    public CalculoImpuestosController(CalculoService calculoService) {
-        this.calculoService = calculoService;
-    }
-
     @PostMapping("/estimar")
     public ResultadoCalculo estimarImpuesto(
             @RequestBody SolicitudCalculo solicitud,
-            @AuthenticationPrincipal Jwt jwt) { // <--- Aquí Spring inyecta el token descifrado
+            @AuthenticationPrincipal Jwt jwt) {
 
         // 1. Extraemos el municipio_id del claim que configuraste en Keycloak
         String municipioIdStr = jwt.getClaimAsString("municipio_id");
